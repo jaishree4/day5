ee5188b2f618e89663debfb863b31bb17aff504c
jaishree4 <44118320+jaishree4@users.noreply.github.com>
2021-11-17 16:55:01 +0530

    ADDED switchWord.sh

---
+++ switchWord.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+read -p "Enter number : [0-9]" digit
+
+
+
+
+    
+    case $digit in
+        0) echo -n "zero " ;;
+        1) echo -n "one " ;;
+        2) echo -n "two " ;;
+        3) echo -n "three " ;;
+        4) echo -n "four " ;;
+        5) echo -n "five " ;;
+        6) echo -n "six " ;;
+        7) echo -n "seven " ;;
+        8) echo -n "eight " ;;
+        9) echo -n "nine " ;;
+    esac
+
