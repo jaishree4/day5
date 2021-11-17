888c2b62ef7da2b266491dfa0721f9fe9d268620
jaishree4 <44118320+jaishree4@users.noreply.github.com>
2021-11-17 16:56:12 +0530

    ADDED word.sh

---
+++ word.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "enter a number"
+read num
+if [  $num -eq 0 ]
+	then
+	echo "0:zero"
+elif [ $num -eq 1 ]
+	then
+	echo "1:One"
+elif [ $num -eq 2 ]
+	then
+	echo "2:Two"
+elif [ $num -eq 3 ]
+	then
+	echo "3:Three"
+elif [ $num -eq 4 ]
+	then
+	echo "4;four"
+elif [ $num -eq 5 ]
+        then
+        echo "5:fiv"
+elif [ $num -eq 6 ]
+        then
+        echo "6:six"
+elif [ $num -eq 7 ]
+        then
+        echo "7:seven"
+elif [ $num -eq 8 ]
+        then
+        echo "8:eight"
+else
+   echo "nine"
+
+
+
+
+fi
